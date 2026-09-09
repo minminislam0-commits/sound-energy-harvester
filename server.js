ef29086db89f5f5d87780d05b0c269baf2b67584
minminislam0-commits <minminislam0@gmail.com>
2026-09-09 14:21:07 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI } from "google-genai";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
